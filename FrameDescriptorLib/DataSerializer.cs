2d389acdfaf9f02d9b2303aa989645c41de68aec
kanyo.tibi@gmail.com <kanyo.tibi@gmail.com@b05c5eed-98d0-a408-09a6-c3e5a8ff8ceb>
2012-10-18 20:30:37 +0000

    ADD: Initial version, but usable

---
+++ FrameDescriptorLib/DataSerializer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace FrameDescriptorLib
+{
+    public class DataSerializer
+    {
+        public static void SaveFrameDescriptor(string filePath, IList<FrameDescriptor> obj)
+        {
+            Save(filePath, obj);
+        }
+
+        public static IList<FrameDescriptor> LoadFrameDescriptor(string filePath)
+        {
+            return Load<IList<FrameDescriptor>>(filePath);
+        }
+
+        public static void Save<T>(string filePath, T obj)
+        {
+            string dir = Path.GetDirectoryName(filePath);
+            if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                try
+                {
+                    var bformatter = new BinaryFormatter();
+                    bformatter.Serialize(stream, obj);
+                }
+                catch (SerializationException e)
+                {
+                    Logger.Logger.WriteLine("Exception in serialization: " + e.Message);
+                }
+            }
+
+        }
+
+        public static T Load<T>(string filePath) where T : class
+        {
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                try
+                {
+                    var bformatter = new BinaryFormatter();
+                    return (T)bformatter.Deserialize(stream);
+                }
+                catch (SerializationException e)
+                {
+                    Logger.Logger.WriteLine("Exception in deserialization: " + e.Message);
+                    return null;
+                }
+            }
+        }
+    }
+}
+
---
+++ LedCubeClient/ClientLogger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LedCubeClient
+{
+    public static class ClientLogger
+    {
+        private static readonly object LockObj = new object();
+        public static List<string> Log = new List<string>();
+
+        public static List<string> GetLog()
+        {
+            lock(LockObj)
+            {
+                var l = new List<string>();
+                foreach(var i in Log)
+                {
+                    l.Add((string)i.Clone());
+                    Log.Remove(i);
+                }
+                return l;
+            }
+        }
+        
+        public static void WriteLine(string message)
+        {
+            Logger.Logger.WriteLine(message);
+        }
+
+        public static void WriteLine(string format, params object[] args)
+        {
+            Logger.Logger.WriteLine(string.Format(format, args));
+        }
+    }
+}
